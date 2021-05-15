e0e9dd05aadc7dc178e823c7f9caccd7a817c573
idiliev18 <idiliev18@codingburgas.bg>
2021-05-15 19:51:46 +0300

    Add problem 14

---
+++ problem_14/problem_14.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    int v;
+    A(int x) : v(x + 1) {}
+    int get() const { return v; }
+};
+
+int main()
+{
+    A a(2);
+    A b(a);
+    cout << a.get() << b.get();
+}
+
+// Answer: 33
