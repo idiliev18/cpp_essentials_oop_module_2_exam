4c8a09bfe8b3476d0cdbe3a3f4e5985b66a44e76
idiliev18 <idiliev18@codingburgas.bg>
2021-05-15 18:38:50 +0300

    Add problem 11

---
+++ problem_11/problem_11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+	const int v;
+	A(int x) : v(x + 1) {}
+	int get() { return ++v; }
+};
+
+int main()
+{
+	A a(2);
+	cout << a.get();
+	return 0;
+}
+
+// Answer: Compilation fails
