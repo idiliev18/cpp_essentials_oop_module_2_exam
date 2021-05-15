bd41928ae8d4f1d431233479a1e6abc68954a879
idiliev18 <idiliev18@codingburgas.bg>
2021-05-15 16:08:11 +0300

    Add problem 2

---
+++ problem_2/problem_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class X {
+protected:
+    int v;
+};
+
+class Y : protected X {
+    Y() :v(0) {}
+};
+
+int main()
+{
+    Y* y = new Y();
+    cout << y->v;
+    delete y;
+    return 0;
+}
+
+// Answer: Compilation fails
